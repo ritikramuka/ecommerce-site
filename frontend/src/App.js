4a1a501ad1842f2c421313e7f48b3a9b1545e576
ritikramuka <ritik.ramuka@gmail.com>
2021-03-17 03:57:16 +0530

    worked on product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <h1>Hello World</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
