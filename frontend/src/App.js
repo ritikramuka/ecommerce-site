cb66c360ef11cd81f1ad424e8e4f08f41827c8be
ritikramuka <ritik.ramuka@gmail.com>
2021-03-17 03:11:58 +0530

    added react-bootstrap & worked on header & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
