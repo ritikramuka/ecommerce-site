01c1dfd3660666596465d65eae44e2c3508728e9
ritikramuka <ritik.ramuka@gmail.com>
2021-05-10 05:54:55 +0530

    worked on product reducers and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,20 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducer } from "./reducers/productReducer";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList: productListReducer,
+});
 
 const intialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-    reducer, 
+    reducer,
     intialState,
-    composeWithDevTools(applyMiddleware(...middleware)),
-)
+    composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
