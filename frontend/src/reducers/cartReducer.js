a6cfa5d046da82cc411e0ca3e928ca905b5f2a8b
ritikramuka <ritik.ramuka@gmail.com>
2021-05-12 02:25:36 +0530

    some more bugs ughh :((

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -4,13 +4,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case CART_ADD_ITEMS:
             let item = action.payload;
-            let itemExist = state.cartItems.find((e) => e.product === item.product);
+            let itemExist = state.cartItems.find((e) => e.productId === item.productId);
             if (itemExist) {
                 return {
                     ...state,
-                    //here e.product === itemExist.product matches the id's of the product
                     cartItems: state.cartItems.map((e) =>
-                        e.product === itemExist.product ? item : e
+                        e.productId === itemExist.productId ? item : e
                     ),
                 };
             } else {
