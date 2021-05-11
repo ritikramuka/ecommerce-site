d22d5015ada0c7c648965a5f0f02a0c591638ae5
ritikramuka <ritik.ramuka@gmail.com>
2021-05-11 07:26:10 +0530

    worked on cartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../actions/cartActions";
+import { Row, Col, ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Message from "../components/Message";
 
-function CartScreen() {
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id;
+
+    const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+
     return (
-        <div>
-            hello from cart
-        </div>
-    )
-}
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 ? (
+                    <Message>
+                        Your Cart Is Empty  <Link to="/">Go Back</Link>
+                    </Message>
+                ) : (
+                    <ListGroup variant="flush"></ListGroup>
+                )}
+            </Col>
+            <Col md={2}></Col>
+            <Col md={2}></Col>
+        </Row>
+    );
+};
 
-export default CartScreen
+export default CartScreen;
