594ca6f33fd317815a42bc98772d2a3c99833255
ritikramuka <ritik.ramuka@gmail.com>
2021-05-11 06:10:33 +0530

    a bug :(

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 function ProductScreen(props) {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
