6469e2c18921a1bc8c2997de9c4c10155e2cb19e
ritikramuka <ritik.ramuka@gmail.com>
2021-05-13 01:46:03 +0530

    some commented info

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,22 @@
 import Product from "../models/productModel.js";
 import asynHandler from "express-async-handler";
 
+/* 
+    @desc Fetch all products
+    @route GET/api/products
+    @access public
+*/
 const getProducts = asynHandler(async (req, res) => {
     const products = await Product.find({});
 
     res.json(products);
 });
 
+/* 
+    @desc Fetch product by id
+    @route GET/api/product/id
+    @access public
+*/
 const getProductById = asynHandler(async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
